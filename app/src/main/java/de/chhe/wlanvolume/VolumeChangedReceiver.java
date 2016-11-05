32cc81ab54ff31bdb49601a3f83b1ddfccff2945
heinzware <ch_1992@hotmail.de>
2016-11-05 13:55:59 +0100

    added launcher icon

--- app/src/main/java/de/chhe/wlanvolume/VolumeChangedReceiver.java
+++ app/src/main/java/de/chhe/wlanvolume/VolumeChangedReceiver.java
@@ -4,6 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+/**
+ * BroadcastReceiver to receive Intents when the volume of the device is changed by the user.
+ * <p>
+ * <b>Note:</b> The action <i>android.media.VOLUME_CHANGED_ACTION</i> is undocumented behavior, so
+ * it is not guaranteed that every device sends it.
+ *
+ */
 public class VolumeChangedReceiver extends BroadcastReceiver {
 
     @Override
@@ -12,7 +19,5 @@ public class VolumeChangedReceiver extends BroadcastReceiver {
         if (intent != null && "android.media.VOLUME_CHANGED_ACTION".equals(intent.getAction())) {
 
         }
-
-        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
