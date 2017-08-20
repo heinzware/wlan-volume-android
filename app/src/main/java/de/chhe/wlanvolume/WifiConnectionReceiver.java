8126f9c7865d8d27cc3172b25da330379e4bdd48
heinzware <ch_1992@hotmail.de>
2017-08-20 12:17:04 +0200

    refactoring WifiConnectionReceiver

--- app/src/main/java/de/chhe/wlanvolume/WifiConnectionReceiver.java
+++ app/src/main/java/de/chhe/wlanvolume/WifiConnectionReceiver.java
@@ -35,107 +35,112 @@ public class WifiConnectionReceiver extends BroadcastReceiver {
         if (intent != null && intent.hasExtra(WifiManager.EXTRA_NETWORK_INFO)) {
 
             NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-            SharedPreferences prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
 
             if (networkInfo.isConnected() && !networkInfo.getExtraInfo().equals(EXTRA_UNKNOWN_SSID)) {
 
                 String ssid = ActivityHelper.trimSsid(networkInfo.getExtraInfo());
 
-                if (ssid != null && (!prefs.contains(PREFERENCE_KEY_CONNECTED) || !prefs.getBoolean(PREFERENCE_KEY_CONNECTED,false))) {
+                //start task to check the network and if it's wanted, to change the volume
+                new WifiConnectedTask(ssid, context).execute();
+                //}
+            } else if (!networkInfo.isConnected() && networkInfo.getState() == NetworkInfo.State.DISCONNECTED
+                    && networkInfo.getDetailedState() == NetworkInfo.DetailedState.DISCONNECTED) {
 
-                    //save in preferences that the device is connected
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.putBoolean(PREFERENCE_KEY_CONNECTED, true);
-                    editor.commit();
+                //restore the old volume, if the user wants it so
+                new AsyncTask<Integer, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Integer... integers) {
+                        SharedPreferences prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+                        if (prefs.contains(PREFERENCE_KEY_CONNECTED)) {
 
-                    //start task to check the network and if it's wanted, to change the volume
-                    new WifiConnectedTask(ssid, context, editor).execute();
-                }
-            } else {
+                            int oldVolume = -1;
+                            final String ssid = prefs.getString(PREFERENCE_KEY_CONNECTED_TO, null);
 
-                if (prefs.contains(PREFERENCE_KEY_CONNECTED)) {
+                            //get the old volume that was set before we changed it
+                            if (prefs.contains(PREFERENCE_KEY_VOL_OLD)) {
+                                oldVolume = prefs.getInt(PREFERENCE_KEY_VOL_OLD, -1);
+                            }
 
-                    int oldVolume = -1;
-                    final String ssid = prefs.getString(PREFERENCE_KEY_CONNECTED_TO, null);
-
-                    //get the old volume that was set before we changed it
-                    if (prefs.contains(PREFERENCE_KEY_VOL_OLD)) {
-                        oldVolume = prefs.getInt(PREFERENCE_KEY_VOL_OLD, -1);
-                    }
-
-                    if (oldVolume != -1 && ssid != null) {
-
-                        //restore the old volume, if the user wants it so
-                        new AsyncTask<Integer, Void, Void>() {
-                            @Override
-                            protected Void doInBackground(Integer... integers) {
+                            if (oldVolume != -1 && ssid != null) {
                                 WifiVolume wifiVolume = DatabaseHelper.getInstance(context).getWifiVolumeBySsid(ssid);
                                 if (wifiVolume != null && wifiVolume.isRestore()) {
                                     AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                                    audioManager.setStreamVolume(AudioManager.STREAM_RING, integers[0], AudioManager.FLAG_VIBRATE);
+                                    audioManager.setStreamVolume(AudioManager.STREAM_RING, oldVolume, AudioManager.FLAG_VIBRATE);
                                     Log.d(TAG, String.format(Locale.getDefault(), "Disconnected from WiFi network \"%s\". Changed volume from %d to %d.", ssid, wifiVolume.getVolume(), integers[0]));
                                 }
-                                return null;
                             }
-                        }.execute(oldVolume);
+
+                            //remove values cause we are not connected anymore
+                            SharedPreferences.Editor editor = prefs.edit();
+                            editor.remove(PREFERENCE_KEY_CONNECTED_TO);
+                            editor.remove(PREFERENCE_KEY_CONNECTED);
+                            editor.remove(PREFERENCE_KEY_VOL_OLD);
+                            editor.commit();
+                            Log.d(TAG, String.format(Locale.getDefault(), "Disconnected from WiFi network \"%s\". Removed preferences.", ssid));
+                        }
+                        return null;
                     }
 
-                    //remove values cause we are not connected anymore
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.remove(PREFERENCE_KEY_CONNECTED_TO);
-                    editor.remove(PREFERENCE_KEY_CONNECTED);
-                    editor.remove(PREFERENCE_KEY_VOL_OLD);
-                    editor.commit();
-                    Log.d(TAG, String.format(Locale.getDefault(), "Disconnected from WiFi network \"%s\". Removed preferences.", ssid));
-
-                    //remove notification
-                    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                    notificationManager.cancel(ActivityHelper.NOTIFICATION_TAG, ActivityHelper.NOTIFICATION_ID);
-                }
+                    @Override
+                    protected void onPostExecute(Void aVoid) {
+                        //remove notification
+                        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                        notificationManager.cancel(ActivityHelper.NOTIFICATION_TAG, ActivityHelper.NOTIFICATION_ID);
+                    }
+                }.execute();
             }
         }
     }
+
 
     private static class WifiConnectedTask extends AsyncTask<Void, Void, Integer> {
 
         private static final int RETURN_CODE_VOLUME_CHANGED           = 0;
         private static final int RETURN_CODE_VOLUME_NOT_CHANGED       = 1;
+        private final SharedPreferences prefs;
 
         private String ssid;
         private Context context;
-        private SharedPreferences.Editor editor;
         private WifiVolume wifiVolume;
         private int maxVolume;
 
-        WifiConnectedTask(@NonNull String ssid, @NonNull Context context, @NonNull SharedPreferences.Editor editor) {
+        WifiConnectedTask(@NonNull String ssid, @NonNull Context context) {
             this.ssid = ssid;
             this.context = context;
-            this.editor = editor;
+            this.prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
         }
 
         @Override
         protected Integer doInBackground(Void... voids) {
+            if (ssid != null && (!prefs.contains(PREFERENCE_KEY_CONNECTED) || !prefs.getBoolean(PREFERENCE_KEY_CONNECTED,false))) {
 
-            //check if the user wants to change the volume, when we are connected to this SSID
-            wifiVolume = DatabaseHelper.getInstance(context).getWifiVolumeBySsid(ssid);
-            if (wifiVolume != null) {
-
-                //get the audio-manager, the max-volume and the current volume
-                AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
-                int oldVolume = audioManager.getStreamVolume(AudioManager.STREAM_RING);
-
-                //set the new volume
-                audioManager.setStreamVolume(AudioManager.STREAM_RING, wifiVolume.getVolume(), AudioManager.FLAG_VIBRATE);
-
-                //save the old volume so we can restore it on disconnect
-                editor.putInt(PREFERENCE_KEY_VOL_OLD, oldVolume);
-                editor.putString(PREFERENCE_KEY_CONNECTED_TO, ssid);
+                //save in preferences that the device is connected
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean(PREFERENCE_KEY_CONNECTED, true);
                 editor.commit();
 
-                Log.d(TAG, String.format(Locale.getDefault(), "Connected to WiFi network \"%s\". Changed volume from %d to %d.", ssid, oldVolume, wifiVolume.getVolume()));
+                //check if the user wants to change the volume, when we are connected to this SSID
+                wifiVolume = DatabaseHelper.getInstance(context).getWifiVolumeBySsid(ssid);
+                if (wifiVolume != null) {
 
-                return RETURN_CODE_VOLUME_CHANGED;
+                    //get the audio-manager, the max-volume and the current volume
+                    AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                    maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
+                    int oldVolume = audioManager.getStreamVolume(AudioManager.STREAM_RING);
+
+                    //set the new volume
+                    //TODO: check if dnd is enabled
+                    audioManager.setStreamVolume(AudioManager.STREAM_RING, wifiVolume.getVolume(), AudioManager.FLAG_VIBRATE);
+
+                    //save the old volume so we can restore it on disconnect
+                    editor.putInt(PREFERENCE_KEY_VOL_OLD, oldVolume);
+                    editor.putString(PREFERENCE_KEY_CONNECTED_TO, ssid);
+                    editor.commit();
+
+                    Log.d(TAG, String.format(Locale.getDefault(), "Connected to WiFi network \"%s\". Changed volume from %d to %d.", ssid, oldVolume, wifiVolume.getVolume()));
+
+                    return RETURN_CODE_VOLUME_CHANGED;
+                }
             }
             return RETURN_CODE_VOLUME_NOT_CHANGED;
         }
